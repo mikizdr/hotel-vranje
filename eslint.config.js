e70a1b293c8e545e77e4b942439e5c722ba1aac5
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2026-09-04 17:05:53 +0200

    Add BaseLayout component and global styles
    - Created a new BaseLayout.astro file to encapsulate the HTML structure and meta tags.
    - Updated index.astro to use the BaseLayout component for rendering the page.
    - Added global.css file to import Tailwind CSS for styling.

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,14 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
 
-export default [...eslintPluginAstro.configs.recommended];
+export default [
+    ...eslintPluginAstro.configs.recommended,
+    {
+        files: ['**/*.astro'],
+        languageOptions: {
+            parserOptions: {
+                parser: '@typescript-eslint/parser',
+                extraFileExtensions: ['.astro'],
+            },
+        },
+    },
+];
